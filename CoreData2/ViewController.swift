28aecda42ff12377bdb8f7c96458dc6f5ca36c97
ramtiga <dhanegm731@gmail.com>
2017-12-16 07:15:17 +0900

    using Core Data

--- CoreData2/ViewController.swift
+++ CoreData2/ViewController.swift
@@ -8,11 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+  @IBOutlet weak var tableView: UITableView!
+  
+  var tasks : [Task] = []
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    tableView.delegate = self
+    tableView.dataSource = self
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    getData()
+    
+    tableView.reloadData()
   }
 
   override func didReceiveMemoryWarning() {
@@ -21,5 +32,52 @@ class ViewController: UIViewController {
   }
 
 
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return tasks.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = UITableViewCell()
+    
+    let task = tasks[indexPath.row]
+    
+    if task.isimportant {
+      cell.textLabel?.text = "★\(task.name!)"
+    } else {
+      cell.textLabel?.text = task.name
+    }
+
+
+    return cell
+  }
+  
+  func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    if editingStyle == .delete {
+      let task = tasks[indexPath.row]
+      context.delete(task)
+      (UIApplication.shared.delegate as! AppDelegate).saveContext()
+      
+      do {
+        tasks = try context.fetch(Task.fetchRequest())
+
+      } catch {
+        print("fetch error")
+        
+      }
+      tableView.reloadData()
+    }
+  }
+  
+  func getData() {
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    do {
+      tasks = try context.fetch(Task.fetchRequest())
+    } catch {
+      print("fetch error")
+    }
+
+  }
 }
 
